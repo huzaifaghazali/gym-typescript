3cbd381a07ee5a163e9a9cb565703df4618b82f2
huzaifaghazali <huzaifaghazali@gmail.com>
2024-06-13 21:20:30 +0500

    Create footer section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from '@/scenes/home';
 import Benefits from '@/scenes/benefits';
 import OurClasses from '@/scenes/ourClasses';
 import ContactUs from './scenes/contactUs';
+import Footer from './scenes/footer';
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -46,6 +47,7 @@ function App() {
         <OurClasses setSelectedPage={setSelectedPage} />
         <OurClasses setSelectedPage={setSelectedPage} />
         <ContactUs setSelectedPage={setSelectedPage} />
+        <Footer />
       </div>
     </>
   );
